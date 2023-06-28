4f89d878009bccdc9cde98bcf4548efb5bf7694f
Test <m-mik@ro.ru>
2023-06-28 18:48:41 +0300

    e

--- src/js/e2e/e2e.server.js
+++ src/js/e2e/e2e.server.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
+// const config = require("../webpack.dev");
 const config = require("D:/homework/js_in_brow/testing/webpack.dev");
 
 const server = new WebpackDevServer(webpack(config), {});
